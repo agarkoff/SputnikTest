772114fc33afa9c5e67cde71becc4266eb346a7a
MisterParser <stas.agarkov@gmail.com>
2018-08-20 11:46:40 +0300

    Изменения для WildFly

--- src/main/java/ru/misterparser/shop/ShopApplication.java
+++ src/main/java/ru/misterparser/shop/ShopApplication.java
@@ -2,9 +2,10 @@ package ru.misterparser.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShopApplication {
+public class ShopApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
